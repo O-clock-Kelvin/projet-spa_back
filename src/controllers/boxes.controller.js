b5c837a9b503809e7b0b84a228cbbab8811bbbe4
Bernard Arroues <arroues.bernard@gmail.com>
2023-01-23 17:16:58 +0100

    Add boxes features

---
+++ src/controllers/boxes.controller.js
@@ -0,0 +1,104 @@
+/** @format */
+
+import prismaClient from '../prisma.js';
+
+const boxesController = {
+	getAll: async (req, res) => {
+		try {
+			const boxes = await prismaClient.box.findMany({});
+			res.json(boxes);
+		} catch (error) {
+			/**
+			 * @todo error handling
+			 */
+			throw new Error(error);
+		}
+	},
+
+	getOne: async (req, res) => {
+		try {
+			const box = await prismaClient.box.findUnique({
+				where: {
+					id: Number(req.params.id),
+				},
+			});
+			res.json(box);
+		} catch (error) {
+			/**
+			 * @todo error handling
+			 */
+			throw new Error(error);
+		}
+	},
+
+	getAnimals: async (req, res) => {
+		try {
+			// on va chercher dans les animaux quels animaux ont l'id de la box
+			const animals = await prismaClient.animal.findMany({
+				where: {
+					box_id: Number(req.params.id),
+				},
+			});
+			res.json(animals);
+		} catch (error) {
+			/**
+			 * @todo error handling
+			 */
+			throw new Error(error);
+		}
+	},
+
+	create: async (req, res) => {
+		try {
+			const createdBox = await prismaClient.box.create({
+				data: {
+					type: req.body.type.toUpperCase() || 'OTHER',
+					nbr_of_places: Number(req.body.nbr_of_places),
+					number: req.body.number,
+				},
+			});
+			res.status(201).json(createdBox);
+		} catch (error) {
+			/**
+			 * @todo error handling
+			 */
+			throw new Error(error);
+		}
+	},
+
+	update: async (req, res) => {
+		try {
+			const updatedBox = await prismaClient.box.update({
+				where: {
+					id: Number(req.params.id),
+				},
+				data: req.body,
+			});
+
+			res.json(updatedBox);
+		} catch (error) {
+			/**
+			 * @todo error handling
+			 */
+			throw new Error(error);
+		}
+	},
+
+	delete: async (req, res) => {
+		try {
+			await prismaClient.box.delete({
+				where: {
+					id: Number(req.params.id),
+				},
+			});
+			res.status(204).json([]);
+		} catch (error) {
+			/**
+			 * @todo error handling
+			 */
+			throw new Error(error);
+		}
+	},
+};
+
+export default boxesController;
--- src/routes/boxes.route.js
+++ src/routes/boxes.route.js
@@ -1,44 +1,47 @@
 /** @format */
 
 import express from 'express';
-// import boxesController from '../controllers/boxes.controller.js';
-
-const boxRouter = express.Router();
+import boxesController from '../controllers/boxes.controller.js';
 
 /**
  * Router qui gère l'ensemble des routes "boxes"
  */
 
+const boxRouter = express.Router();
+
 /**
  * Récupère la liste de toutes les boxes
  * => on pourra appliquer un middleware de filtre
  */
-// router.get('/boxes');
 
-/**
- * Récupère une box
- */
-// router.get('/boxes/:id');
+boxRouter.get('/', boxesController.getAll);
 
 /* je veux connaitre les animaux d'une box
  */
 // router.get('/boxes/:id/animals');
 
+boxRouter.get('/:id/animals', boxesController.getAnimals);
+
+/**
+ * Récupère une box
+ */
+boxRouter.get('/:id', boxesController.getOne);
+
 /**
  * Créer un nouveau box
  * => pas utile pour la v1
  */
-// router.post('/boxes')
+boxRouter.post('/', boxesController.create);
 
 /**
  * Mettre à jour un box
  * => pas utile pour la v1
  */
-// router.update('/boxes/:id')
+boxRouter.patch('/:id', boxesController.update);
 
 /**
  * Supprimer un box
  */
-// router.delete('/boxes/:id')
+boxRouter.delete('/:id', boxesController.delete);
 
 export default boxRouter;
