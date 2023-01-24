4a993b2d75988ee4d79f37ff7932a525608a80e7
angelique pinchon <ange.pinchon@gmail.com>
2023-01-24 11:23:31 +0100

    add animal features

--- src/index.js
+++ src/index.js
@@ -7,11 +7,13 @@ import v1Router from './routes/router.js';
 // on charge les variables d'environnement
 dotenv.config();
 const app = express();
+
 app.use(express.json());
 
 // on commence a écouter sur le port défini dans les variables d'env
 app.listen(process.env.PORT, () => {
 	console.log(`SERVER STARTED ON PORT ${process.env.PORT}`);
 });
+
 
 app.use('/v1', v1Router);
--- src/routes/router.js
+++ src/routes/router.js
@@ -2,10 +2,13 @@
 import express from 'express';
 import userRouter from './users.route.js';
 import authRouter from './auth.route.js';
+import animalRouter from './animals.route.js';
 
 const v1Router = express.Router();
 
 v1Router.use('/users', userRouter);
+
+v1Router.use('/animals', animalRouter);
 
 v1Router.use('/auth', authRouter);
 
